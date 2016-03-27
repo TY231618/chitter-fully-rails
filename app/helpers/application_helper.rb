2a3d09a74fd40a615a126b50eb5f98dbab4068cb
Tony Young <ty_training@hotmail.co.uk>
2016-03-27 20:42:06 +0100

    Added method to application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Chitter App"
+    if page_title.empty?
+      return base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Chitter App"
+    assert_select "title", "Chitter App"
   end
 
   test "should get help" do
