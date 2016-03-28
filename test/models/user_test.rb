beb4cb872a81e8d9e6511a648d75236ff658835a
Tony Young <ty_training@hotmail.co.uk>
2016-03-28 14:11:48 +0100

    added bcrypt and password validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'example', username: 'example123', email: 'example@mail.com')
+    @user = User.new(name: 'example', username: 'example123', email: 'example@mail.com',
+                    password: "password", password_confirmation: "password")
   end
 
   test 'should be valid' do
@@ -62,5 +63,15 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test 'password should not be blank' do
+    @user.password = @user.password_confirmation = '' * 6
+    assert_not @user.valid?
+  end
+
+  test 'password should have a minimum length' do
+    @user.password = @user.password_confirmation = 'a' * 5
+    assert_not @user.valid?
   end
 end
