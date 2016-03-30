9f6efb288cbdf5481c40ac03a42459c267658e1f
Tony Young <ty_training@hotmail.co.uk>
2016-03-30 20:59:17 +0100

    used faker gem and paginated index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name:  "Example User",
+             username: "123example123",
+             email: "example@mail.com",
+             password:              "123456",
+             password_confirmation: "123456")
+
+99.times do |n|
+  name  = Faker::Name.name
+  username = "example#{n+1}"
+  email = "example-#{n+1}@mail.com"
+  password = "password"
+  User.create!(name:  name,
+               username: username,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
